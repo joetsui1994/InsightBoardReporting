3f3620e00259f481cd0fdaaaed895f9f32af5862
joetsui1994 <joetsui1994@gmail.com>
2024-09-26 23:56:19 +0100

    now allow time-series plotting by epiweek

--- plots/time_series_barplot/preprocess.py
+++ plots/time_series_barplot/preprocess.py
@@ -1,21 +1,13 @@
 import pandas as pd
+from datetime import datetime, timedelta
 
 def preprocess_time_series_data(data, parameters):
     """
     Preprocesses data for the time-series bar plot.
     """
     time_col = parameters.get('time_col')
-    moving_average_window = parameters.get('moving_average_window', 1)
-
-    # validate moving_average_window
-    try:
-        moving_average_window = int(moving_average_window)
-        if moving_average_window < 1:
-            print(f"Invalid moving_average_window '{moving_average_window}'. Defaulting to 1.")
-            moving_average_window = 1
-    except (ValueError, TypeError):
-        print(f"Invalid moving_average_window '{moving_average_window}'. Defaulting to 1.")
-        moving_average_window = 1
+    aggregate_by_epiweek = parameters.get('aggregate_by_epiweek', False)
+    moving_average_window = parameters.get('moving_average_window')
 
     # check that time_col is in data
     if time_col not in data.columns:
@@ -24,17 +16,24 @@ def preprocess_time_series_data(data, parameters):
     # create a copy of relevant column
     plot_data = data[[time_col]].copy()
 
-    # convert time_col to datetime and extract date
-    try:
+    # convert time_col to datetime 
+    plot_data[time_col] = pd.to_datetime(plot_data[time_col])
+
+    # aggregate by epiweek if specified
+    if aggregate_by_epiweek:
+        # extract epiweek from the date using the isocalendar() method
+        plot_data['year'], plot_data['epiweek'], _ = plot_data[time_col].dt.isocalendar().values.T
+        plot_data = plot_data.groupby(['year', 'epiweek']).size().reset_index(name='count')
+        # add start of each epiweek as date
+        plot_data['date'] = plot_data.apply(lambda x: datetime.strptime('%d-W%d-1' % (x['year'], x['epiweek']), "%G-W%V-%u") - timedelta(days=1), axis=1)
+    else:
         plot_data['date'] = pd.to_datetime(plot_data[time_col]).dt.date
-    except Exception as e:
-        raise ValueError(f"Error converting '{time_col}' to datetime: {e}")
-    plot_data = plot_data.groupby('date').size().reset_index(name='count')
+        plot_data = plot_data.groupby('date').size().reset_index(name='count')
 
     # add 0 count for missing dates
-    all_dates = pd.date_range(plot_data['date'].min(), plot_data['date'].max(), freq='D')
-    all_dates = all_dates.date
-    all_dates_df = pd.DataFrame(all_dates, columns=['date'])
+    all_dates = pd.date_range(plot_data['date'].min(), plot_data['date'].max(), freq='D' if not aggregate_by_epiweek else 'W')
+    all_dates_df = pd.DataFrame(all_dates.date, columns=['date'])
+    all_dates_df['date'] = pd.to_datetime(all_dates_df['date'])
     # merge
     plot_data = all_dates_df.merge(plot_data, how='left', on='date')
     plot_data['count'].fillna(0, inplace=True)
